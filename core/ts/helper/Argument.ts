8339c83add88ebe1b4b8446dde0201e0e2fa27f6
MSay2 <yoannmeclot@hotmail.com>
2020-06-24 16:16:02 +0200

    Add Snackbar widget
    Add license on all typescript files

--- core/ts/helper/Argument.ts
+++ core/ts/helper/Argument.ts
@@ -1,9 +1,25 @@
 /**
  * @ Author: Yoann Meclot. MSay2
  * @ Modified on: 2020-05-27 14:39:02
- * @ Modified on: 2020-06-24 10:44:11
+ * @ Modified on: 2020-06-24 14:59:40
  * 
  * @version 1.3
+ */
+
+/**
+   Copyright (c) 2020 MSay2 - Support-typescript
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
  */
 
 /**
--- core/ts/helper/Callable.ts
+++ core/ts/helper/Callable.ts
@@ -1,9 +1,25 @@
 /**
  * @ Author: Yoann Meclot. MSay2
  * @ Created on: 2020-05-27 10:49:40
- * @ Modified on: 2020-06-22 11:14:17
+ * @ Modified on: 2020-06-24 14:59:34
  * 
  * @version 1.0
+ */
+
+/**
+   Copyright (c) 2020 MSay2 - Support-typescript
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
  */
 
  /**
